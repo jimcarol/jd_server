f42309bc017f43a1532562351279aa1633ce3678
jimhe <jimhe@appledeMac-mini.local>
2018-01-03 18:47:54 +0800

    delete

--- pagination-plugin/app/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react"
-import ReactDOM from "react-dom"
-import TwoColorBall from "./two_color_ball.js"
-
-ReactDOM.render( 
-  <TwoColorBall />,
-  document.getElementById("root")
-)
